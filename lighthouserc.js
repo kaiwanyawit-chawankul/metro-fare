7b48e04957eea1955d249dbd5a014c43daaf4948
Kaiwanyawit Chawankul <kaiwanyawit.c@thoughtworks.com>
2021-11-24 20:57:18 +0700

    add lighthouse

---
+++ lighthouserc.js
@@ -0,0 +1,21 @@
+module.exports = {
+    ci: {
+      collect: {
+        url: [
+          "http://127.0.0.1:3000"
+        ],
+        startServerCommand: "yarn start",
+        startServerReadyPattern: "Available on",
+        numberOfRuns: 1
+      },
+      assert: {
+        preset: "lighthouse:recommended",
+      },
+      upload: {
+        /* Add configuration here */
+        target: 'lhci',
+        serverBaseUrl: 'https://peaceful-earth-32428.herokuapp.com/',
+        token: 'LHCI_TOKEN', // could also use LHCI_TOKEN variable instead
+      },
+    },
+  };
